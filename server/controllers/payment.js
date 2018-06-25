72936c7138ecf79cc439691d9a0ba93d6a1b23a2
heysyamy <32830072+heysyamy@users.noreply.github.com>
2018-06-25 14:51:22 +0800

    update

--- server/controllers/payment.js
+++ server/controllers/payment.js
@@ -36,5 +36,51 @@ exports.list = function(req, res) {
     });
 };
 // edit payment details
+exports.edit = function(req, res) {
+    var user = req.session.passport.user;
+    var detail_num = req.params.id;
+    paymentModel.findById(detail_num).then(function(paymentDetails){
+        res.render("editPaymentDetails", {
+            title: "Edit Payment Details",
+            item: paymentDetails,
+            hostPath: req.protocol + "://" + req.get("host"),
+            user: user,
+            avatar: require('gravatar').url(user.email, {s: '100', r: 'x', d: 'retro'}, true)
+        });
+    }).catch((err)=> {
+        return res.status(400).send({
+            message: err
+        });
+    });
+}
 // update payment details
+exports.update = function(req, res) {
+    var detail_num = req.params.id;
+    var updateDetails = {
+        cardHolderName: req.body.cardHolderName,
+        cardNumber: req.body.cardNumber,
+        securityCode: req.body.securityCode,
+        expiryDate: req.body.expiryDate
+    }
+    paymentModel.update(updateDetails, {where: {id: detail_num}}).then((updatedDetails)=> {
+        if (!updatedDetails || updatedDetails==0) {
+            return res.send(400, {
+                message: "error"
+            });
+        }
+        res.status(200).send({message: "Updated Payment Details: " + detail_num});
+    })
+};
 // delete payment details
+exports.delete = function(req, res) {
+    var detail_num = req.params.id;
+    console.log("Deleting" + detail_num);
+    paymentModel.destroy({where: {id: detail_num}}).then((deleteDetail)=> {
+        if (!deleteDetail){
+            return res.send(400, {
+                message: "error"
+            });
+        }
+        res.status(200).send({message: "Delete Payment Detail: " + detail_num});
+    });
+}
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -5,6 +5,9 @@ var paymentController = require("../controllers/payment");
 // define the home page route
 paymentRouter.post("/", isLoggedIn, paymentController.insert);
 paymentRouter.get("/", isLoggedIn, paymentController.list);
+paymentRouter.get("/edit/:id", isLoggedIn, paymentController.edit);
+paymentRouter.post("/edit/:id", isLoggedIn, paymentController.update);
+paymentRouter.delete("/:id", isLoggedIn, paymentController.delete);
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
