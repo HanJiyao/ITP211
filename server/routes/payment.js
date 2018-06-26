24d80f49a1f3aed58cd06ae2c082cc2f75f0ace8
heysyamy <32830072+heysyamy@users.noreply.github.com>
2018-06-26 15:19:55 +0800

    update and debug payment

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -5,8 +5,8 @@ var paymentController = require("../controllers/payment");
 // define the home page route
 paymentRouter.post("/", isLoggedIn, paymentController.insert);
 paymentRouter.get("/", isLoggedIn, paymentController.list);
-paymentRouter.get("/edit/:id", isLoggedIn, paymentController.edit);
-paymentRouter.post("/edit/:id", isLoggedIn, paymentController.update);
+paymentRouter.get("/editDetails/:id", isLoggedIn, paymentController.edit);
+paymentRouter.post("/editDetails/:id", isLoggedIn, paymentController.update);
 paymentRouter.delete("/:id", isLoggedIn, paymentController.delete);
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
