777362cad3e083c57d2b4c001767f21a2d64fa57
heysyamy <32830072+heysyamy@users.noreply.github.com>
2018-07-03 15:58:24 +0800

    wallet

--- server/models/Wallet.js
+++ server/models/Wallet.js
@@ -3,6 +3,23 @@ module.exports = function (sequelize, Sequelize) {
         balance: {
             type: Sequelize.INTEGER
         },
-    })
+        //need any other attributes?
+        userID: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        },
+    });
+    Wallet.sync({force: true, logging: console.log}).then(() => {
+        console.log("Wallet table synced");
+        Wallet.upsert({
+            //test data
+            balance: 500.00,
+            userID: 1
+        })
+    });
     return Wallet;
 }
