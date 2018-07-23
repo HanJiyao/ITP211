0a06ae463119155126a00f6efb25e018ef999318
HanJiyao <hanjiyao0@gmail.com>
2018-07-23 15:07:58 +0800

    validate checkout

--- server/controllers/checkout.js
+++ server/controllers/checkout.js
@@ -8,29 +8,26 @@ exports.show = (req, res) => {
     });
     models.sequelize.query(
         'select u.address address, u.postal_code postal_code,\
-        w.balance balance,\
         c.id id, p.id productID, p.productImage productImage, p.productName productName, p.productType productType, p.price price, c.quantity quantity, p.quantity prodQuantity\
         from Carts c\
         join Products p on c.productID = p.id\
-        join Wallets w on c.userID = w.userID\
         join Users u on c.userID = u.id\
-        where c.checked=1 and c.userID='+user.id+' and w.userID='+user.id, {model: models.Cart}
+        where c.checked=1 and c.userID='+user.id, {model: models.Cart}
     ).then((cartData) => {
-        res.render("checkout", {
-            title: "Check Out",
-            cartData: cartData,
-            user: user,
-            cartNum: cartNum,
-            avatar: require('gravatar').url(user.email, {
-                s: '100',
-                r: 'x',
-                d: 'retro'
-            }, true),
-            hostPath: req.protocol + "://" + req.get("host"),
+        models.Wallet.findOrCreate({ where: { userID: user.id } }).then((wallet)=>{
+            res.render("checkout", {
+                title: "Check Out",
+                cartData: cartData,
+                wallet:wallet,
+                user: user,
+                cartNum: cartNum,
+                avatar: require('gravatar').url(user.email, {
+                    s: '100',
+                    r: 'x',
+                    d: 'retro'
+                }, true),
+                hostPath: req.protocol + "://" + req.get("host"),
+            });
         });
-    }).catch((err) => {
-        return res.status(400).send({
-            message: err
-        });
-    });
+    })
 }
