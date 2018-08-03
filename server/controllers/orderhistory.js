4ec735308f96fb047358a777db63d80cfe5b2eb1
cheeebai <173976P@mymail.nyp.edu.sg>
2018-08-03 17:51:07 +0800

    Auto stash before merge of "master" and "origin/master"
    FK KM

--- server/controllers/orderhistory.js
+++ server/controllers/orderhistory.js
@@ -5,7 +5,7 @@ exports.list=function(req,res){
     var cartNum = 0;
     models.sequelize.query('select count(*) cartNum from Carts where userID =' + user.id + '', {model: models.Cart}).then((data) => {cartNum = data[0].dataValues.cartNum});
     //list all orders and sort by date
-    models.sequelize.query("select *, od.quantity quantity from OrderDetails od inner join Products p on od.productID=p.id inner join Orders o on od.orderID=o.id", {model: models.OrderDetails})
+    models.sequelize.query("select *,od.productID, od.quantity quantity from OrderDetails od inner join Products p on od.productID=p.id inner join Orders o on od.orderID=o.id", {model: models.OrderDetails})
     .then((orderhistory)=>{
         console.log(orderhistory)
         res.render("orderhistory",{
@@ -14,7 +14,7 @@ exports.list=function(req,res){
             user:user,
             cartNum: cartNum,
             avatar: avatar = require('gravatar').url(user.email, {s: '100', r: 'x', d: 'retro'}, true),
-            urlPath:req.protocol+"://" + req.get("host")+req.url,
+            urlPath:req.protocol+"://" + req.get("host"),
         })
     }).catch((err)=>{
         return res.status(400).send({
@@ -22,7 +22,29 @@ exports.list=function(req,res){
         });
     });
 };
-
+exports.listdetail=function(req,res){
+    var user=req.session.passport.user;
+    // get cart num
+    var cartNum = 0;
+    models.sequelize.query('select count(*) cartNum from Carts where userID ='+user.id, {model: models.Cart}).then((data) => {cartNum = data[0].dataValues.cartNum});
+    var orderID = req.params.id;
+    models.sequelize.query("select *,od.productID, od.quantity quantity from OrderDetails od inner join Products p on od.productID=p.id inner join Orders o on od.orderID=o.id where od.orderID="+orderID, {model: models.OrderDetails})
+    .then((orderhistory)=>{
+        console.log(orderhistory)
+        res.render("orderdetails",{
+            title:"Order Details page",
+            orderhistory:orderhistory,
+            user:user,
+            cartNum: cartNum,
+            avatar: avatar = require('gravatar').url(user.email, {s: '100', r: 'x', d: 'retro'}, true),
+            urlPath:req.protocol+"://" + req.get("host"),
+        })
+    }).catch((err)=>{
+        return res.status(400).send({
+            message:err
+        });
+    });
+};
 exports.hasAuthorization=function(req,res,next){
     if (req.isAuthenticated())
         return next();
